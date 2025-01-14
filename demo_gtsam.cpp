80b41015a092f9eab677c740554d710f09deb0bf
Paul Taylor <123190211+uni-paul-taylor2@users.noreply.github.com>
2025-01-14 10:11:33 -0400

    Update demo_gtsam.cpp

--- demo_gtsam.cpp
+++ demo_gtsam.cpp
@@ -1,5 +1,7 @@
-//g++ demo_gtsam.cpp -o demo_gtsam.o
+//g++ -I/usr/local/include/gtsam -I/usr/local/include/gtsam/3rdparty/Eigen demo_gtsam.cpp -o demo_gtsam.o
 
+#include <Eigen/Core>
+#include <gtsam/global_includes.h>
 #include <gtsam/geometry/Pose3.h>
 #include <gtsam/inference/Symbol.h>
 #include <gtsam/geometry/Point3.h>
@@ -10,21 +12,17 @@ using namespace std;
 using namespace gtsam;
 
 int main() {
-    // Define a Pose3 object (3D pose: translation and rotation)
+    cout<<"GTSAM Eigen: "<<GTSAM_EIGEN_VERSION_WORLD<<'.'<<GTSAM_EIGEN_VERSION_MAJOR<<".0"<<endl;
+    cout<<"Eigen: "<<EIGEN_WORLD_VERSION<<'.'<<EIGEN_MAJOR_VERSION<<'.'<<EIGEN_MINOR_VERSION<<endl;
+    
     Point3 translation(1.0, 2.0, 3.0);  // 3D translation (x, y, z)
     Rot3 rotation = Rot3::RzRyRx(0.0, 0.0, 0.0);  // No rotation (identity matrix)
 
-    // Create the pose (rotation + translation)
     Pose3 pose(rotation, translation);
-
-    // Output the original pose
-    cout << "Original Pose: " << pose << endl;
-
-    // Apply a translation to the pose (example transformation)
+    //cout << "Original Pose: " << pose << endl;
+    
     Pose3 transformedPose = pose.compose(Pose3(Rot3(), Point3(1.0, 0.0, 0.0)));
-
-    // Output the transformed pose
-    cout << "Transformed Pose: " << transformedPose << endl;
+    //cout << "Transformed Pose: " << transformedPose << endl;
 
     return 0;
 }
