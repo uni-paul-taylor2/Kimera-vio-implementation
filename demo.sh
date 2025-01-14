8de4de72df41709dfa9d678dd1df43e4eb1eeef5
Paul Taylor <123190211+uni-paul-taylor2@users.noreply.github.com>
2025-01-14 10:54:00 -0400

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -1,10 +1,4 @@
 # not finished yet ;-;
-cd $HOME/cmake_repositories/Kimera-VIO
-mkdir $HOME/datasets
-cd $HOME/datasets
-wget https://github.com/ethz-asl/kimera-vio/raw/main/datasets/tum_rgbd_bag/tum_rgbd_bagfile.bag
-rosbag play ~/datasets/tum_rgbd_bagfile.bag
-rostopic list
-rosnode list
-g++ demo.cpp -o demo.o
-./demo.o
+g++ src/demo.cpp -o src/demo.o
+# this would be replaced with proper cmake shenanigans
+./src/demo.o
