1cbfa12b03da9f733d8f4e431357d58dc070453d
Paul Taylor <123190211+uni-paul-taylor2@users.noreply.github.com>
2025-01-15 18:07:44 -0400

    a few more dependencies

--- reset.sh
+++ reset.sh
@@ -8,9 +8,9 @@ for dir in *; do
   fi
 done
 
-sudo apt purge -y libboost-all-dev build-essential libtbb-dev libgflags-dev libgoogle-glog-dev libavcodec-dev libavformat-dev libswscale-dev
-sudo apt purge -y unzip pkg-config libjpeg-dev libpng-dev libtiff-dev libvtk7-dev libgtk-3-dev libparmetis-dev libatlas-base-dev gfortran
-sudo apt purge -y ffmpeg python3-dev python3-numpy libeigen3-dev libopencv-dev libopencv-contrib-dev
+sudo apt purge -y libboost-all-dev build-essential libtbb-dev libgflags-dev libgoogle-glog-dev libavcodec-dev libavformat-dev libswscale-dev \
+  unzip pkg-config libjpeg-dev libpng-dev libtiff-dev libvtk7-dev libgtk-3-dev libparmetis-dev libatlas-base-dev gfortran ffmpeg \
+  python3-dev python3-numpy libeigen3-dev libopencv-dev libopencv-contrib-dev libmetis-dev xvfb python3 python3-pip python3-tk
 
 cd ..
 sudo rm -rf cmake_repositories
