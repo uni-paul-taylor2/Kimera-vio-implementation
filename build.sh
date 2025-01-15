37cb71c9a56c23fe0ecb436fd831d875b34d6d50
Paul Taylor <123190211+uni-paul-taylor2@users.noreply.github.com>
2025-01-15 18:10:29 -0400

    a few more dependencies

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-sudo apt install -y libboost-all-dev build-essential libtbb-dev libgflags-dev libgoogle-glog-dev libavcodec-dev libavformat-dev libswscale-dev
-sudo apt install -y unzip pkg-config libjpeg-dev libpng-dev libtiff-dev libvtk7-dev libgtk-3-dev libparmetis-dev libatlas-base-dev gfortran
-sudo apt install -y ffmpeg python3-dev python3-numpy libeigen3-dev libopencv-dev libopencv-contrib-dev
+sudo apt install -y libboost-all-dev build-essential libtbb-dev libgflags-dev libgoogle-glog-dev libavcodec-dev libavformat-dev libswscale-dev \
+  unzip pkg-config libjpeg-dev libpng-dev libtiff-dev libvtk7-dev libgtk-3-dev libparmetis-dev libatlas-base-dev gfortran ffmpeg \
+  python3-dev python3-numpy libeigen3-dev libopencv-dev libopencv-contrib-dev libmetis-dev xvfb python3 python3-pip python3-tk
 
 cd $HOME
 mkdir cmake_repositories
