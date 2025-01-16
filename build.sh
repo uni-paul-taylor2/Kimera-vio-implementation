5a3d460936b73953acf55cd42d791e39f6f255f7
Paul Taylor <123190211+uni-paul-taylor2@users.noreply.github.com>
2025-01-16 10:53:53 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+sudo apt-get update
+sudo apt upgrade
 sudo apt install -y libboost-all-dev build-essential libtbb-dev libgflags-dev libgoogle-glog-dev libavcodec-dev libavformat-dev libswscale-dev \
   unzip pkg-config libjpeg-dev libpng-dev libtiff-dev libvtk7-dev libgtk-3-dev libparmetis-dev libatlas-base-dev gfortran ffmpeg \
   python3-dev python3-numpy libeigen3-dev libopencv-dev libopencv-contrib-dev libmetis-dev xvfb python3 python3-pip python3-tk
