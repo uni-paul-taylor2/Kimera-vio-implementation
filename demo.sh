11ddeb78824e8c0017ab9455c514402071f9c67d
Paul Taylor <123190211+uni-paul-taylor2@users.noreply.github.com>
2025-01-16 21:52:48 -0400

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -1,4 +1,6 @@
-# not finished yet ;-;
-g++ src/demo.cpp -o src/demo.o
-# this would be replaced with proper cmake shenanigans
-./src/demo.o
+mkdir build
+cd build
+cmake ..
+make -j$(nproc)
+./demo --vio_params=/path/to/VioParams.yaml --dataset_path=/path/to/dataset/
+#almost there, just to tweak the params to make sense in this and the cpp
